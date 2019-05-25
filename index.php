8130676cf0309e8a8291f18fd537a69ec867d75b
decka <decka@github.com>
2019-05-25 14:57:57 +0200

    new 3

--- index.php
+++ index.php
@@ -73,48 +73,16 @@
         <p>Inspire the community</p>
     </span>
     <span class="benefits-image">
-    <img src="/assets/Image%209.png" alt="">
+
     </span>
     </div>
 </div>
 </section>
 
 
-<section class="featured-section">
-
-   <img class="banner" src="assets/SVG/gaa-banner.svg" alt="">
-
-     <div class="carousel">
-    <div class="carousel-track-container">
-        <ul class="carousel-track">
-            <li class="carousel-slide current-slide"><img classs="carousel-image"src="assets/tess5.jpg" alt=""></li>
-            <li class="carousel-slide"><img classs="carousel-image"src="assets/tess6.jpg" alt=""></li>
-            <li class="carousel-slide"><img classs="carousel-image"src="assets/tess7.jpg" alt=""></li>
-        </ul>
-    </div>
-
-
-
-
-
-    <div class="carousel-nav">
-    <button class="carousel-button carousel-button-left">
-        <img src="assets/SVG/slide-left.svg" alt="">
-    </button>
-    <img  class="thunder-icon-2" src="assets/SVG/thunder-icon.svg" alt="">
-    <button class="carousel-button carousel-button-right">
-        <img src="assets/SVG/slide-right.svg" alt="">
-    </button>
-     </div>
-</div>
-
-</section>
-
-
-
 
 <section class="how-section">
-    <div class="banner">
+    <div class="how-banner">
         <section>
          <div class="group">
     <div class="row">
@@ -141,14 +109,21 @@
       <span>HOW</span>
       <span>HOW</span>
       <span>HOW</span>
-
-
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
+      <span>HOW</span>
     </div>
-
+</div>
+</section>
 </div>
 
-        </section>
-    </div>
+
+
 
     <!-- Slideshow container -->
 <div class="wrap">
@@ -187,106 +162,46 @@ SELECT APPLICANTS </div>
 </section>
 
 
+<section class="featured-section">
+
+   <img class="banner" src="assets/SVG/gaa-banner.svg" alt="">
+
+     <div class="carousel">
+    <div class="carousel-track-container">
+        <ul class="carousel-track">
+            <li class="carousel-slide current-slide"><img classs="carousel-image"src="assets/tess5.jpg" alt=""></li>
+            <li class="carousel-slide"><img classs="carousel-image"src="assets/tess6.jpg" alt=""></li>
+            <li class="carousel-slide"><img classs="carousel-image"src="assets/tess7.jpg" alt=""></li>
+        </ul>
+    </div>
+
+
+
+
+
+    <div class="carousel-nav">
+    <button class="carousel-button carousel-button-left">
+        <img src="assets/SVG/slide-left.svg" alt="">
+    </button>
+    <img  class="thunder-icon-2" src="assets/SVG/thunder-icon.svg" alt="">
+    <button class="carousel-button carousel-button-right">
+        <img src="assets/SVG/slide-right.svg" alt="">
+    </button>
+     </div>
+</div>
+
+</section>
+
+
+
+
 <section class="collaborate-section">
 
 <div class="collaborate-image">
-   <img src="assets/46522819_2043789555664339_4940279402010247168_o.jpg" alt="">
+
 </div>
 <div class="form-wrapper">
 
-
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
 
 
 </div>
